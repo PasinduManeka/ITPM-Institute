2c8f3785013745d9c33fde7c8fbad753f98049eb
PasinduManeka <manekaherat815@gmail.com>
2021-04-02 14:33:38 +0530

    create the retrieve query and insert query

---
+++ ABC Institute/ABC Institute/Classes/GroupClass.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABC_Institute.Classes
+{
+    class GroupClass
+    {
+        //Getters and Setters
+        public string yearSemester { get; set; }
+        public string programme { get; set; }
+        public int groupchNumber { get; set; }
+        public int subgroupNumber { get; set; }
+        public string groupID { get; set; }
+        public string subGroupID { get; set; }
+
+        static string myconnString =ConfigurationManager.ConnectionStrings["connection"].ConnectionString;
+
+        //selecting data from database
+        public DataTable Select()
+        {
+            //Connection
+            SqlConnection con = new SqlConnection(myconnString);
+            DataTable dt = new DataTable();
+            try
+            {
+                //Sql quesry
+                String sql = "SELECT* FROM student_groups";
+                //create connection
+                SqlCommand cmd = new SqlCommand(sql,con);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                con.Open();
+                adapter.Fill(dt);
+            }
+            catch(Exception e)
+            {
+
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            return dt;
+        }
+
+        //Inserting data to Database
+        public bool Insert(GroupClass gc)
+        {
+            //Creating default return type and setting it's value to false
+            bool isSuccess = false;
+
+            //Connect Databse
+            SqlConnection con = new SqlConnection(myconnString);
+            try
+            {
+                //SQL query to data insert
+                String sql = "INSERT INTO student_groups (academicyearsemester, programme, groupNumber, subGroupNumber, subGroupId, groupID) VALUES (@academicyearsemester, @programme, @groupNumber, @subGroupNumber, @subGroupId, @groupID)";
+
+                //Creating SQL Command using sql and conn
+                SqlCommand cmd = new SqlCommand(sql, con);
+
+                cmd.Parameters.AddWithValue("@academicyearsemester", gc.yearSemester);
+                cmd.Parameters.AddWithValue("@programme", gc.programme);
+                cmd.Parameters.AddWithValue("@groupNumber", gc.groupchNumber);
+                cmd.Parameters.AddWithValue("@subGroupNumber", gc.subgroupNumber);
+                cmd.Parameters.AddWithValue("@subGroupId", gc.subGroupID);
+                cmd.Parameters.AddWithValue("@groupID", gc.groupID);
+
+                //Connection open here
+                con.Open();
+                int rows = cmd.ExecuteNonQuery();
+
+                if (rows>0)
+                {
+                    isSuccess = true;
+                }
+                else
+                {
+                    isSuccess = false;
+                }
+
+            }
+            catch(Exception e)
+            {
+
+            }
+            finally
+            {
+                con.Close();
+            }
+            return isSuccess;
+        } 
+
+
+    }
+}
