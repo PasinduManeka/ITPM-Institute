c1e4033de161229af967069e115596e56164317b
PasinduManeka <manekaherat815@gmail.com>
2021-05-20 16:35:22 +0530

    set parallel session

--- ABC Institute/ABC Institute/Form5.Designer.cs
+++ ABC Institute/ABC Institute/Form5.Designer.cs
@@ -31,26 +31,24 @@ namespace ABC_Institute
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.lblMsgDidaplay = new System.Windows.Forms.Label();
             this.btnViewConsecutive = new System.Windows.Forms.Button();
             this.btnSaveConsecutive = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.chk = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.btnViewParallel = new System.Windows.Forms.Button();
             this.btnSaveParallel = new System.Windows.Forms.Button();
+            this.btnSaveParalle = new System.Windows.Forms.Button();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.chk1 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             this.tabPage3 = new System.Windows.Forms.TabPage();
-            this.btnViewOverlapping = new System.Windows.Forms.Button();
-            this.btnSaveOverlapping = new System.Windows.Forms.Button();
-            this.dataGridView3 = new System.Windows.Forms.DataGridView();
             this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.lblDisplayMSG2 = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.tabPage2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
-            this.tabPage3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -59,35 +57,41 @@ namespace ABC_Institute
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Controls.Add(this.tabPage3);
             this.tabControl1.Controls.Add(this.tabPage4);
-            this.tabControl1.Location = new System.Drawing.Point(1, 0);
-            this.tabControl1.Margin = new System.Windows.Forms.Padding(4);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1368, 499);
+            this.tabControl1.Size = new System.Drawing.Size(1062, 557);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
             // 
+            this.tabPage1.BackColor = System.Drawing.Color.White;
+            this.tabPage1.Controls.Add(this.lblMsgDidaplay);
             this.tabPage1.Controls.Add(this.btnViewConsecutive);
             this.tabPage1.Controls.Add(this.btnSaveConsecutive);
             this.tabPage1.Controls.Add(this.dataGridView1);
-            this.tabPage1.Location = new System.Drawing.Point(4, 27);
-            this.tabPage1.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage1.ForeColor = System.Drawing.Color.Black;
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(4);
-            this.tabPage1.Size = new System.Drawing.Size(1360, 468);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1054, 528);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Consecutive";
-            this.tabPage1.UseVisualStyleBackColor = true;
-            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // lblMsgDidaplay
+            // 
+            this.lblMsgDidaplay.AutoSize = true;
+            this.lblMsgDidaplay.Location = new System.Drawing.Point(352, 455);
+            this.lblMsgDidaplay.Name = "lblMsgDidaplay";
+            this.lblMsgDidaplay.Size = new System.Drawing.Size(0, 17);
+            this.lblMsgDidaplay.TabIndex = 3;
             // 
             // btnViewConsecutive
             // 
-            this.btnViewConsecutive.BackColor = System.Drawing.Color.DarkOrange;
-            this.btnViewConsecutive.Location = new System.Drawing.Point(643, 371);
-            this.btnViewConsecutive.Margin = new System.Windows.Forms.Padding(4);
+            this.btnViewConsecutive.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnViewConsecutive.Location = new System.Drawing.Point(631, 353);
             this.btnViewConsecutive.Name = "btnViewConsecutive";
-            this.btnViewConsecutive.Size = new System.Drawing.Size(180, 54);
+            this.btnViewConsecutive.Size = new System.Drawing.Size(136, 65);
             this.btnViewConsecutive.TabIndex = 2;
             this.btnViewConsecutive.Text = "View";
             this.btnViewConsecutive.UseVisualStyleBackColor = false;
@@ -95,12 +99,11 @@ namespace ABC_Institute
             // btnSaveConsecutive
             // 
             this.btnSaveConsecutive.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.btnSaveConsecutive.Location = new System.Drawing.Point(323, 371);
-            this.btnSaveConsecutive.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSaveConsecutive.Location = new System.Drawing.Point(263, 353);
             this.btnSaveConsecutive.Name = "btnSaveConsecutive";
-            this.btnSaveConsecutive.Size = new System.Drawing.Size(180, 54);
+            this.btnSaveConsecutive.Size = new System.Drawing.Size(136, 65);
             this.btnSaveConsecutive.TabIndex = 1;
-            this.btnSaveConsecutive.Text = "Save";
+            this.btnSaveConsecutive.Text = "Add Session";
             this.btnSaveConsecutive.UseVisualStyleBackColor = false;
             this.btnSaveConsecutive.Click += new System.EventHandler(this.btnSaveConsecutive_Click);
             // 
@@ -110,12 +113,11 @@ namespace ABC_Institute
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.chk});
-            this.dataGridView1.Location = new System.Drawing.Point(36, 21);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Location = new System.Drawing.Point(39, 39);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(1165, 289);
+            this.dataGridView1.Size = new System.Drawing.Size(973, 262);
             this.dataGridView1.TabIndex = 0;
             // 
             // chk
@@ -127,142 +129,118 @@ namespace ABC_Institute
             // 
             // tabPage2
             // 
-            this.tabPage2.Controls.Add(this.btnViewParallel);
+            this.tabPage2.Controls.Add(this.lblDisplayMSG2);
             this.tabPage2.Controls.Add(this.btnSaveParallel);
+            this.tabPage2.Controls.Add(this.btnSaveParalle);
             this.tabPage2.Controls.Add(this.dataGridView2);
-            this.tabPage2.Location = new System.Drawing.Point(4, 27);
-            this.tabPage2.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(4);
-            this.tabPage2.Size = new System.Drawing.Size(1360, 468);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1054, 528);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Parallel";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // btnViewParallel
-            // 
-            this.btnViewParallel.BackColor = System.Drawing.Color.DarkOrange;
-            this.btnViewParallel.Location = new System.Drawing.Point(656, 351);
-            this.btnViewParallel.Margin = new System.Windows.Forms.Padding(4);
-            this.btnViewParallel.Name = "btnViewParallel";
-            this.btnViewParallel.Size = new System.Drawing.Size(180, 54);
-            this.btnViewParallel.TabIndex = 4;
-            this.btnViewParallel.Text = "View";
-            this.btnViewParallel.UseVisualStyleBackColor = false;
-            // 
             // btnSaveParallel
             // 
-            this.btnSaveParallel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.btnSaveParallel.Location = new System.Drawing.Point(336, 351);
-            this.btnSaveParallel.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSaveParallel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnSaveParallel.Location = new System.Drawing.Point(632, 349);
             this.btnSaveParallel.Name = "btnSaveParallel";
-            this.btnSaveParallel.Size = new System.Drawing.Size(180, 54);
-            this.btnSaveParallel.TabIndex = 3;
-            this.btnSaveParallel.Text = "Save";
+            this.btnSaveParallel.Size = new System.Drawing.Size(136, 65);
+            this.btnSaveParallel.TabIndex = 5;
+            this.btnSaveParallel.Text = "View";
             this.btnSaveParallel.UseVisualStyleBackColor = false;
+            // 
+            // btnSaveParalle
+            // 
+            this.btnSaveParalle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnSaveParalle.Location = new System.Drawing.Point(264, 349);
+            this.btnSaveParalle.Name = "btnSaveParalle";
+            this.btnSaveParalle.Size = new System.Drawing.Size(136, 65);
+            this.btnSaveParalle.TabIndex = 4;
+            this.btnSaveParalle.Text = "Add Session";
+            this.btnSaveParalle.UseVisualStyleBackColor = false;
+            this.btnSaveParalle.Click += new System.EventHandler(this.btnSaveParalle_Click);
             // 
             // dataGridView2
             // 
+            this.dataGridView2.AllowUserToAddRows = false;
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(34, 7);
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.chk1});
+            this.dataGridView2.Location = new System.Drawing.Point(39, 35);
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.RowHeadersWidth = 51;
             this.dataGridView2.RowTemplate.Height = 24;
-            this.dataGridView2.Size = new System.Drawing.Size(1163, 289);
-            this.dataGridView2.TabIndex = 0;
+            this.dataGridView2.Size = new System.Drawing.Size(973, 262);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // chk1
+            // 
+            this.chk1.HeaderText = "";
+            this.chk1.MinimumWidth = 6;
+            this.chk1.Name = "chk1";
+            this.chk1.Width = 125;
             // 
             // tabPage3
             // 
-            this.tabPage3.Controls.Add(this.btnViewOverlapping);
-            this.tabPage3.Controls.Add(this.btnSaveOverlapping);
-            this.tabPage3.Controls.Add(this.dataGridView3);
-            this.tabPage3.Location = new System.Drawing.Point(4, 27);
-            this.tabPage3.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(1360, 468);
+            this.tabPage3.Size = new System.Drawing.Size(1054, 528);
             this.tabPage3.TabIndex = 2;
-            this.tabPage3.Text = "Overlapping";
+            this.tabPage3.Text = "Non Overlapping";
             this.tabPage3.UseVisualStyleBackColor = true;
-            // 
-            // btnViewOverlapping
-            // 
-            this.btnViewOverlapping.BackColor = System.Drawing.Color.DarkOrange;
-            this.btnViewOverlapping.Location = new System.Drawing.Point(675, 348);
-            this.btnViewOverlapping.Margin = new System.Windows.Forms.Padding(4);
-            this.btnViewOverlapping.Name = "btnViewOverlapping";
-            this.btnViewOverlapping.Size = new System.Drawing.Size(180, 54);
-            this.btnViewOverlapping.TabIndex = 6;
-            this.btnViewOverlapping.Text = "View";
-            this.btnViewOverlapping.UseVisualStyleBackColor = false;
-            // 
-            // btnSaveOverlapping
-            // 
-            this.btnSaveOverlapping.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.btnSaveOverlapping.Location = new System.Drawing.Point(355, 348);
-            this.btnSaveOverlapping.Margin = new System.Windows.Forms.Padding(4);
-            this.btnSaveOverlapping.Name = "btnSaveOverlapping";
-            this.btnSaveOverlapping.Size = new System.Drawing.Size(180, 54);
-            this.btnSaveOverlapping.TabIndex = 5;
-            this.btnSaveOverlapping.Text = "Save";
-            this.btnSaveOverlapping.UseVisualStyleBackColor = false;
-            // 
-            // dataGridView3
-            // 
-            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView3.Location = new System.Drawing.Point(38, 15);
-            this.dataGridView3.Name = "dataGridView3";
-            this.dataGridView3.RowHeadersWidth = 51;
-            this.dataGridView3.RowTemplate.Height = 24;
-            this.dataGridView3.Size = new System.Drawing.Size(1163, 289);
-            this.dataGridView3.TabIndex = 1;
             // 
             // tabPage4
             // 
-            this.tabPage4.Location = new System.Drawing.Point(4, 27);
-            this.tabPage4.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage4.Location = new System.Drawing.Point(4, 25);
             this.tabPage4.Name = "tabPage4";
-            this.tabPage4.Size = new System.Drawing.Size(1360, 468);
+            this.tabPage4.Size = new System.Drawing.Size(1054, 528);
             this.tabPage4.TabIndex = 3;
-            this.tabPage4.Text = "Non Available Times";
+            this.tabPage4.Text = "Not Available Times";
             this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // lblDisplayMSG2
+            // 
+            this.lblDisplayMSG2.AutoSize = true;
+            this.lblDisplayMSG2.Location = new System.Drawing.Point(239, 458);
+            this.lblDisplayMSG2.Name = "lblDisplayMSG2";
+            this.lblDisplayMSG2.Size = new System.Drawing.Size(0, 17);
+            this.lblDisplayMSG2.TabIndex = 6;
             // 
             // Form5
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1244, 498);
+            this.ClientSize = new System.Drawing.Size(1087, 581);
             this.Controls.Add(this.tabControl1);
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form5";
-            this.Text = "Sessions and Not Available Time Allocations ";
-            this.Load += new System.EventHandler(this.Form5_Load);
+            this.Load += new System.EventHandler(this.Form5_Load_1);
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
-            this.tabPage3.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.Button btnViewConsecutive;
-        private System.Windows.Forms.Button btnSaveConsecutive;
-        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.TabPage tabPage3;
         private System.Windows.Forms.TabPage tabPage4;
-        private System.Windows.Forms.Button btnViewParallel;
-        private System.Windows.Forms.Button btnSaveParallel;
-        private System.Windows.Forms.DataGridView dataGridView2;
-        private System.Windows.Forms.Button btnViewOverlapping;
-        private System.Windows.Forms.Button btnSaveOverlapping;
-        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Label lblMsgDidaplay;
+        private System.Windows.Forms.Button btnViewConsecutive;
+        private System.Windows.Forms.Button btnSaveConsecutive;
+        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridViewCheckBoxColumn chk;
+        private System.Windows.Forms.Button btnSaveParallel;
+        private System.Windows.Forms.Button btnSaveParalle;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn chk1;
+        private System.Windows.Forms.Label lblDisplayMSG2;
     }
 }
--- ABC Institute/ABC Institute/Form5.cs
+++ ABC Institute/ABC Institute/Form5.cs
@@ -21,7 +21,7 @@ namespace ABC_Institute
         GroupClass c = new GroupClass();
         Session s = new Session();
        
-
+        //ignore this
         private void Form5_Load(object sender, EventArgs e)
         {
             AddHeaderCheckBox();
@@ -29,7 +29,9 @@ namespace ABC_Institute
 
 
             DataTable dt = s.select();
+            DataTable dt2 = s.select();
             dataGridView1.DataSource = dt;
+            
         }
 
         //header check
@@ -40,6 +42,7 @@ namespace ABC_Institute
         {
             HeaderCheckBox.Size = new Size(15, 15);
             this.dataGridView1.Controls.Add(HeaderCheckBox);
+            this.dataGridView2.Controls.Add(HeaderCheckBox);
         }
         
         //header check click event
@@ -51,6 +54,11 @@ namespace ABC_Institute
                 ((DataGridViewCheckBoxCell)Row.Cells["chk"]).Value = HcheckBox.Checked;
             }
             dataGridView1.RefreshEdit();
+            foreach (DataGridViewRow Row in dataGridView2.Rows)
+            {
+                ((DataGridViewCheckBoxCell)Row.Cells["chk1"]).Value = HcheckBox.Checked;
+            }
+            dataGridView2.RefreshEdit();
             IsHeaderCheckBox = false;
         }
 
@@ -65,12 +73,126 @@ namespace ABC_Institute
             
         }
 
-        private void btnSaveConsecutive_Click(object sender, EventArgs e)
+        private void Form5_Load_1(object sender, EventArgs e)
         {
-            //details get from selected check boxes.
+            AddHeaderCheckBox();
+            HeaderCheckBox.MouseClick += new MouseEventHandler(HeaderCheckBox_MouseClick);
 
 
+            DataTable dt = s.select();
+            dataGridView1.DataSource = dt;
+            dataGridView2.DataSource = dt;
 
         }
+
+        private void btnSaveConsecutive_Click(object sender, EventArgs e)
+        {
+            String sessionID = "";
+            String lectureName = "";
+            String subjectCode = "";
+            String subjectName = "";
+            String groupNumber = "";
+            String Tags = "";
+            string msg = "";
+            string msg2 = "";
+
+            for (int i = 0; i <= dataGridView1.RowCount - 1; i++)
+            {
+                if (Convert.ToBoolean(dataGridView1.Rows[i].Cells["chk"].Value) == true)
+                {
+                    sessionID = dataGridView1.Rows[i].Cells[1].Value.ToString();
+                    lectureName = dataGridView1.Rows[i].Cells[2].Value.ToString();
+                    Tags = dataGridView1.Rows[i].Cells[4].Value.ToString();
+                    groupNumber = dataGridView1.Rows[i].Cells[5].Value.ToString();
+                    subjectName = dataGridView1.Rows[i].Cells[6].Value.ToString();
+                    subjectCode = dataGridView1.Rows[i].Cells[7].Value.ToString();
+
+
+                    msg += sessionID + "," + lectureName + "," + Tags + "," + groupNumber + "," + subjectName + "," + subjectCode;
+                    lblMsgDidaplay.Text = msg;
+                }
+                
+
+
+            }
+
+            //groupNumber = dataGridView1.Rows[rowI].Cells[5].Value.ToString();
+            //subjectName = dataGridView1.Rows[i].Cells[6].Value.ToString();
+            //subjectCode = dataGridView1.Rows[i].Cells[7].Value.ToString();
+
+          
+
+        }
+
+        private void btnSaveParalle_Click(object sender, EventArgs e)
+        {
+            String sessionID = "";
+            String lectureName = "";
+            String subjectCode = "";
+            String subjectName = "";
+            String groupNumber = "";
+            String Tags = "";
+            string msg = "";
+            string msg2 = "";
+
+            for (int i = 0; i <= dataGridView2.RowCount - 1; i++)
+            {
+
+                if (Convert.ToBoolean(dataGridView2.Rows[i].Cells["chk1"].Value) == true)
+                {
+                    sessionID = dataGridView2.Rows[i].Cells[1].Value.ToString();
+                    lectureName = dataGridView2.Rows[i].Cells[2].Value.ToString();
+                    Tags = dataGridView2.Rows[i].Cells[4].Value.ToString();
+                    groupNumber = dataGridView2.Rows[i].Cells[5].Value.ToString();
+                    subjectName = dataGridView2.Rows[i].Cells[6].Value.ToString();
+                    subjectCode = dataGridView2.Rows[i].Cells[7].Value.ToString();
+
+
+                    msg += sessionID + "," + lectureName + "," + Tags + "," + groupNumber + "," + subjectName + "," + subjectCode;
+                    lblDisplayMSG2.Text = msg;
+                }
+
+
+
+            }
+        }
+
+        
+        /*
+private void btnSaveConsecutive_Click(object sender, CheckBox e)
+{
+//details get from selected check boxes.
+String sessionID = "";
+String lectureName = "";
+String subjectCode = "";
+String subjectName = "";
+String groupNumber = "";
+String Tags = "";
+string msg = "";
+string msg2 = "";
+int rowIndex = e.;
+
+sessionID = dataGridView1.Rows[i].Cells[1].Value.ToString();
+lectureName = dataGridView1.Rows[i].Cells[2].Value.ToString();
+
+//Details set on to label
+for (int i=0;i<=dataGridView1.RowCount - 1;i++)
+{
+if (Convert.ToBoolean(dataGridView1.Rows[i].Cells["chk"].Value) == true)
+{
+Tags = dataGridView1.Rows[i].Cells[4].Value.ToString();
+msg +=  Tags+"," ;
+}
+}
+
+groupNumber = dataGridView1.Rows[rowI].Cells[5].Value.ToString();
+subjectName = dataGridView1.Rows[i].Cells[6].Value.ToString();
+subjectCode = dataGridView1.Rows[i].Cells[7].Value.ToString();
+
+msg2 = sessionID+","+ lectureName + ","+ msg + ","+ groupNumber + ","+ subjectName + ","+ subjectCode + ",";
+
+
+
+}*/
     }
 }
